3e855093987f8a5797f4fcc0ca6c5a829e094e92
cdmi1234 <ghadiyayash59@gmail.com>
2023-10-03 17:49:21 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  print("object");
   runApp(MaterialApp(
     home: first(),
   ));
